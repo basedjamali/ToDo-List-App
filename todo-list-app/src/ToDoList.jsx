157d6df832bf6e3a0ccae9239321f444cf368436
Based <basedjamali@gmail.com>
2024-12-30 17:18:08 +0100

    Completed the React functionalites for the App

--- todo-list-app/src/ToDoList.jsx
+++ todo-list-app/src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 const ToDoList = () => {
-  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   const handleInputChange = (e) => {
@@ -9,19 +9,32 @@ const ToDoList = () => {
   }
 
   const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks(t => [...t, newTask])
+      setNewTask("")
+    }
 
   }
 
   const deleteTask = (index) => {
-
+    const updatedTasks = tasks.filter((_, i) => i !== index)
+    setTasks(updatedTasks)
   }
 
   const moveTaskUp = (index) => {
-    
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks)
+    }
   }
 
   const moveTaskDown = (index) => {
-
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks)
+    }
   }
 
   return(
